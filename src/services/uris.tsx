82b42ed5b1360a7a3b659428f73635c43fc187eb
Luis Cabezas <luis@youtouch.cl>
2020-09-20 14:49:57 -0300

    Change base services url

--- src/services/uris.tsx
+++ src/services/uris.tsx
@@ -1,3 +1,3 @@
-export const BASE_API_URL = 'http://api.mercadopublico.cl/servicios/v1/publico'
+export const BASE_API_URL = process.env.BASE_SERVICE_URL
 
 export const API_BUY_ORDERS = BASE_API_URL + '/ordenesdecompra.json'
